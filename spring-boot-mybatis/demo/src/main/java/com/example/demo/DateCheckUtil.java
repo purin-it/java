f5aa6c096afe421936f2efbb371a7af41cd2a107
purin-it <stanahas@amber.plala.or.jp>
2020-05-06 13:20:21 +0900

    spring-boot-mybatis

---
+++ spring-boot-mybatis/demo/src/main/java/com/example/demo/DateCheckUtil.java
@@ -0,0 +1,127 @@
+package com.example.demo;
+
+import java.time.LocalDate;
+import java.time.chrono.JapaneseChronology;
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
+import java.util.Locale;
+
+public class DateCheckUtil {
+
+    /**
+     * 日付チェック処理を行う
+     * @param year　年
+     * @param month 月
+     * @param day 日
+     * @return 判定結果(1:年が空、2:月が空、3:日が空、
+     *          4:年月日が不正、5:年月日が未来日、0:正常)
+     */
+    public static int checkDate(String year, String month, String day){
+        final String dateFormat = "uuuuMMdd";
+        if(isEmpty(year)){
+            return 1;
+        }
+        if(isEmpty(month)){
+            return 2;
+        }
+        if(isEmpty(day)){
+            return 3;
+        }
+        String dateStr = year + addZero(month) + addZero(day);
+        if(!isCorrectDate(dateStr, dateFormat)){
+            return 4;
+        }
+        if(isFutureDate(dateStr, dateFormat)){
+            return 5;
+        }
+        return 0;
+    }
+
+    /**
+     * DateTimeFormatterを利用して日付チェックを行う
+     * @param dateStr チェック対象文字列
+     * @param dateFormat 日付フォーマット
+     * @return 日付チェック結果
+     */
+    private static boolean isCorrectDate(String dateStr, String dateFormat){
+        if(isEmpty(dateStr) || isEmpty(dateFormat)){
+            return false;
+        }
+        //暦体系は和暦体系で、日付と時刻を厳密に解決するスタイルで、
+        //DateTimeFormatterオブジェクトを作成
+        DateTimeFormatter df = DateTimeFormatter.ofPattern(dateFormat)
+                .withChronology(JapaneseChronology.INSTANCE)
+                .withResolverStyle(ResolverStyle.STRICT);
+        try{
+            //チェック対象文字列をLocalDate型の日付に変換できれば、チェックOKとする
+            LocalDate.parse(dateStr, df);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    /**
+     * 日付の文字列が未来日かどうかを判定する
+     * @param dateStr チェック対象文字列
+     * @param dateFormat 日付フォーマット
+     * @return 判定結果
+     */
+    private static boolean isFutureDate(String dateStr, String dateFormat){
+        if(!isCorrectDate(dateStr, dateFormat)){
+            return false;
+        }
+        LocalDate dateStrDate = convertStrToLocalDate(dateStr, dateFormat);
+        LocalDate now = LocalDate.now();
+        if(dateStrDate.isAfter(now)){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 日付の文字列を日付型に変換した結果を返す
+     * @param dateStr 日付の文字列
+     * @param dateFormat 日付のフォーマット
+     * @return 変換後の文字列
+     */
+    private static LocalDate convertStrToLocalDate(String dateStr, String dateFormat){
+        if(isEmpty(dateStr) || isEmpty(dateFormat)){
+            return null;
+        }
+        //暦体系は和暦体系で、日付と時刻を厳密に解決するスタイルで、
+        //DateTimeFormatterオブジェクトを作成
+        DateTimeFormatter df = DateTimeFormatter.ofPattern(dateFormat, Locale.JAPAN)
+                .withChronology(JapaneseChronology.INSTANCE)
+                .withResolverStyle(ResolverStyle.STRICT);
+        //日付の文字列をLocalDate型に変換して返却
+        return LocalDate.parse(dateStr, df);
+    }
+
+    /**
+     * 数値文字列が1桁の場合、頭に0を付けて返す
+     * @param intNum 数値文字列
+     * @return 変換後数値文字列
+     */
+    private static String addZero(String intNum){
+        if(isEmpty(intNum)){
+            return intNum;
+        }
+        if(intNum.length() == 1){
+            return "0" + intNum;
+        }
+        return intNum;
+    }
+
+    /**
+     * 引数の文字列がnull、空文字かどうかを判定する
+     * @param str チェック対象文字列
+     * @return 文字列チェック結果
+     */
+    public static boolean isEmpty(String str){
+        if(str == null || "".equals(str)){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ spring-boot-mybatis/demo/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,270 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.validation.BindingResult;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Controller
+@SessionAttributes(types = {DemoForm.class})
+public class DemoController {
+
+    /**
+     * ユーザーデータテーブル(user_data)へアクセスするマッパー
+     */
+    @Autowired
+    private UserDataMapper mapper;
+
+    /**
+     * ユーザーデータテーブル(user_data)のデータを取得して返却する
+     * @return ユーザーデータリスト
+     */
+    @ModelAttribute("demoFormList")
+    public List<DemoForm> userDataList(){
+        List<DemoForm> demoFormList = new ArrayList<>();
+        return demoFormList;
+    }
+
+    /**
+     * Formオブジェクトを初期化して返却する
+     * @return Formオブジェクト
+     */
+    @ModelAttribute("demoForm")
+    public DemoForm createDemoForm(){
+        DemoForm demoForm = new DemoForm();
+        return demoForm;
+    }
+
+    /**
+     * 初期表示(一覧)画面に遷移する
+     * @param model Modelオブジェクト
+     * @return 一覧画面へのパス
+     */
+    @RequestMapping("/")
+    public String index(Model model){
+        List<DemoForm> demoFormList = new ArrayList<>();
+        //ユーザーデータテーブル(user_data)から全データを取得する
+        Collection<UserData> userDataList = mapper.findAll();
+        for(UserData userData : userDataList){
+            demoFormList.add(getDemoForm(userData));
+        }
+        //ユーザーデータリストを更新
+        model.addAttribute("demoFormList", demoFormList);
+        return "list";
+    }
+
+    /**
+     * 更新処理を行う画面に遷移する
+     * @param id 更新対象のID
+     * @param model Modelオブジェクト
+     * @return 入力・更新画面へのパス
+     */
+    @RequestMapping("/update")
+    public String update(@RequestParam("id") String id, Model model){
+        UserData userData = mapper.findById(Long.parseLong(id));
+        model.addAttribute("demoForm", getDemoForm(userData));
+        return "input";
+    }
+
+    /**
+     * 削除確認画面に遷移する
+     * @param id 更新対象のID
+     * @param model Modelオブジェクト
+     * @return 削除確認画面へのパス
+     */
+    @RequestMapping("/delete_confirm")
+    public String delete_confirm(@RequestParam("id") String id, Model model){
+        UserData userData = mapper.findById(Long.parseLong(id));
+        model.addAttribute("demoForm", getDemoForm(userData));
+        return "confirm_delete";
+    }
+
+    /**
+     * 削除処理を行う
+     * @param demoForm Formオブジェクト
+     * @param model Modelオブジェクト
+     * @return 一覧画面の表示処理
+     */
+    @RequestMapping(value = "/delete", params = "next")
+    @Transactional(readOnly = false)
+    public String delete(DemoForm demoForm, Model model){
+        mapper.deleteById(Long.parseLong(demoForm.getId()));
+        return index(model);
+    }
+
+    /**
+     * 削除確認画面から一覧画面に戻る
+     * @param model Modelオブジェクト
+     * @return 一覧画面
+     */
+    @RequestMapping(value = "/delete", params = "back")
+    public String confirmDeleteBack(Model model){
+        return index(model);
+    }
+
+    /**
+     * 追加処理を行う画面に遷移する
+     * @param model Modelオブジェクト
+     * @return 入力・更新画面へのパス
+     */
+    @RequestMapping("/add")
+    public String add(Model model){
+        model.addAttribute("demoForm", new DemoForm());
+        return "input";
+    }
+
+    /**
+     * エラーチェックを行い、エラーが無ければ確認画面に遷移し、
+     * エラーがあれば入力画面のままとする
+     * @param demoForm Formオブジェクト
+     * @param result バインド結果
+     * @return 確認画面または入力画面へのパス
+     */
+    @RequestMapping(value = "/confirm", params = "next")
+    public String confirm(@Valid DemoForm demoForm, BindingResult result){
+        //formオブジェクトのチェック処理を行う
+        if(result.hasErrors()){
+            //エラーがある場合は、入力画面のままとする
+            return "input";
+        }
+        //生年月日の日付チェック処理を行い、画面遷移する
+        return checkDate(demoForm, result);
+    }
+
+    /**
+     * 一覧画面に戻る
+     * @param model Modelオブジェクト
+     * @return 一覧画面の表示処理
+     */
+    @RequestMapping(value = "/confirm", params = "back")
+    public String confirmBack(Model model){
+        return index(model);
+    }
+
+    /**
+     * 完了画面に遷移する
+     * @param demoForm Formオブジェクト
+     * @return 完了画面
+     */
+    @RequestMapping(value = "/send", params = "next")
+    @Transactional(readOnly = false)
+    public String send(DemoForm demoForm){
+        //更新・追加処理を行うエンティティを生成
+        UserData userData = getUserData(demoForm);
+        //追加・更新処理
+        if(demoForm.getId() == null){
+            userData.setId(mapper.findMaxId() + 1);
+            mapper.create(userData);
+        }else{
+            mapper.update(userData);
+        }
+        return "complete";
+    }
+
+    /**
+     * 入力画面に戻る
+     * @return 入力画面
+     */
+    @RequestMapping(value = "/send", params = "back")
+    public String sendBack(){
+        return "input";
+    }
+
+    /**
+     * 生年月日の日付チェック処理を行い、画面遷移先を返却
+     * @param demoForm Formオブジェクト
+     * @param result バインド結果
+     * @return 確認画面または入力画面へのパス
+     */
+    private String checkDate(DemoForm demoForm, BindingResult result){
+        //生年月日の日付チェック処理を行う
+        //エラーがある場合は、エラーメッセージ・エラーフィールドの設定を行い、
+        //入力画面のままとする
+        int checkDate = DateCheckUtil.checkDate(demoForm.getBirthYear()
+                , demoForm.getBirthMonth(), demoForm.getBirthDay());
+        switch(checkDate){
+            case 1:
+                //生年月日_年が空文字の場合のエラー処理
+                result.rejectValue("birthYear", "validation.date-empty"
+                        , new String[]{"生年月日_年"}, "");
+                return "input";
+            case 2:
+                //生年月日_月が空文字の場合のエラー処理
+                result.rejectValue("birthMonth", "validation.date-empty"
+                        , new String[]{"生年月日_月"}, "");
+                return "input";
+            case 3:
+                //生年月日_日が空文字の場合のエラー処理
+                result.rejectValue("birthDay", "validation.date-empty"
+                        , new String[]{"生年月日_日"}, "");
+                return "input";
+            case 4:
+                //生年月日の日付が不正な場合のエラー処理
+                result.rejectValue("birthYear", "validation.date-invalidate");
+                //生年月日_月・生年月日_日は、エラーフィールドの設定を行い、
+                //メッセージを空文字に設定している
+                result.rejectValue("birthMonth", "validation.empty-msg");
+                result.rejectValue("birthDay", "validation.empty-msg");
+                return "input";
+            case 5:
+                //生年月日の日付が未来日の場合のエラー処理
+                result.rejectValue("birthYear", "validation.date-future");
+                //生年月日_月・生年月日_日は、エラーフィールドの設定を行い、
+                //メッセージを空文字に設定している
+                result.rejectValue("birthMonth", "validation.empty-msg");
+                result.rejectValue("birthDay", "validation.empty-msg");
+                return "input";
+            default:
+                //formオブジェクト・生年月日の日付のチェック処理を行い、
+                //問題なければ確認画面に遷移
+                return "confirm";
+        }
+    }
+
+    /**
+     * DemoFormオブジェクトに引数のユーザーデータの各値を設定する
+     * @param userData ユーザーデータ
+     * @return DemoFormオブジェクト
+     */
+    private DemoForm getDemoForm(UserData userData){
+        DemoForm demoForm = new DemoForm();
+        demoForm.setId(String.valueOf(userData.getId()));
+        demoForm.setName(userData.getName());
+        demoForm.setBirthYear(String.valueOf(userData.getBirthY()));
+        demoForm.setBirthMonth(String.valueOf(userData.getBirthM()));
+        demoForm.setBirthDay(String.valueOf(userData.getBirthD()));
+        demoForm.setSex(userData.getSex());
+        demoForm.setSex_value(userData.getSex_value());
+        return demoForm;
+    }
+
+    /**
+     * UserDataオブジェクトに引数のフォームの各値を設定する
+     * @param demoForm DemoFormオブジェクト
+     * @return ユーザーデータ
+     */
+    private UserData getUserData(DemoForm demoForm){
+        UserData userData = new UserData();
+        if(!DateCheckUtil.isEmpty(demoForm.getId())){
+            userData.setId(Long.valueOf(demoForm.getId()));
+        }
+        userData.setName(demoForm.getName());
+        userData.setBirthY(Integer.valueOf(demoForm.getBirthYear()));
+        userData.setBirthM(Integer.valueOf(demoForm.getBirthMonth()));
+        userData.setBirthD(Integer.valueOf(demoForm.getBirthDay()));
+        userData.setSex(demoForm.getSex());
+        userData.setSex_value(demoForm.getSex_value());
+        return userData;
+    }
+
+}
