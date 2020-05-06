17ef18467c8c015d07e5acb6ddec0fab74eaafc9
purin-it <stanahas@amber.plala.or.jp>
2020-05-06 11:46:13 +0900

    spring-boot-web-check2

---
+++ spring-boot-web-check2/demo/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,123 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * コントローラクラス
+ * 「@SessionAttributes(types = DemoForm.class)」により、
+ * 生成したFormオブジェクトをセッションとしてもたせている
+ */
+@Controller
+@SessionAttributes(types = DemoForm.class)
+public class DemoController {
+
+    /**
+     * Formオブジェクトを初期化して返却する
+     * @return Formオブジェクト
+     */
+    @ModelAttribute("demoForm")
+    public DemoForm createDemoForm(){
+        DemoForm demoForm = new DemoForm();
+        return demoForm;
+    }
+
+    /**
+     * 入力画面に遷移する
+     * @return 入力画面へのパス
+     */
+    @RequestMapping("/")
+    public String index(){
+        return "input";
+    }
+
+    /**
+     * エラーチェックを行い、エラーが無ければ確認画面に遷移し、
+     * エラーがあれば入力画面のままとする
+     * @param demoForm Formオブジェクト
+     * @param result バインド結果
+     * @return 確認画面または入力画面へのパス
+     */
+    @RequestMapping("/confirm")
+    public String confirm(@Valid DemoForm demoForm, BindingResult result){
+        //formオブジェクトのチェック処理を行う
+        if(result.hasErrors()){
+            //エラーがある場合は、入力画面のままとする
+            return "input";
+        }
+        //生年月日の日付チェック処理を行い、画面遷移する
+        return checkDate(demoForm, result);
+    }
+
+    /**
+     * 生年月日の日付チェック処理を行い、画面遷移先を返却
+     * @param demoForm Formオブジェクト
+     * @param result バインド結果
+     * @return 確認画面または入力画面へのパス
+     */
+    private String checkDate(DemoForm demoForm, BindingResult result){
+        //生年月日の日付チェック処理を行う
+        //エラーがある場合は、エラーメッセージ・(エラー時に赤反転するための)
+        //エラーフィールドの設定を行い、入力画面のままとする
+        int checkDate = DateCheckUtil.checkDate(demoForm.getBirthYear()
+                , demoForm.getBirthMonth(), demoForm.getBirthDay());
+        switch(checkDate){
+            case 1:
+                //生年月日_年が空文字の場合のエラー処理
+                result.rejectValue("birthYear", "validation.date-empty"
+                        , new String[]{"生年月日_年"}, "");
+                return "input";
+            case 2:
+                //生年月日_月が空文字の場合のエラー処理
+                result.rejectValue("birthMonth", "validation.date-empty"
+                        , new String[]{"生年月日_月"}, "");
+                return "input";
+            case 3:
+                //生年月日_日が空文字の場合のエラー処理
+                result.rejectValue("birthDay", "validation.date-empty"
+                        , new String[]{"生年月日_日"}, "");
+                return "input";
+            case 4:
+                //生年月日の日付が不正な場合のエラー処理
+                result.rejectValue("birthYear", "validation.date-invalidate");
+                //生年月日_月・生年月日_日は、エラーフィールドの設定を行い、
+                //メッセージを空文字に設定している
+                result.rejectValue("birthMonth", "validation.empty-msg");
+                result.rejectValue("birthDay", "validation.empty-msg");
+                return "input";
+            case 5:
+                //生年月日の日付が未来日の場合のエラー処理
+                result.rejectValue("birthYear", "validation.date-future");
+                //生年月日_月・生年月日_日は、エラーフィールドの設定を行い、
+                //メッセージを空文字に設定している
+                result.rejectValue("birthMonth", "validation.empty-msg");
+                result.rejectValue("birthDay", "validation.empty-msg");
+                return "input";
+            default:
+                //formオブジェクト・生年月日の日付のチェック処理を行い、
+                //問題なければ確認画面に遷移
+                return "confirm";
+        }
+    }
+
+    /**
+     * 完了画面に遷移する
+     * @return 完了画面
+     */
+    @RequestMapping(value = "/send", params = "next")
+    public String send(){
+        return "complete";
+    }
+
+    /**
+     * 入力画面に戻る
+     * @return 入力画面
+     */
+    @RequestMapping(value = "/send", params = "back")
+    public String back(){
+        return "input";
+    }
+}
