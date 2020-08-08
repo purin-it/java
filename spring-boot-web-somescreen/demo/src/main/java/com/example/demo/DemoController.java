3ed71580d98f060166de9005f2aa50f225806bbc
purin-it <stanahas@amber.plala.or.jp>
2020-08-08 13:46:55 +0900

    spring-boot-web-somescreen

--- spring-boot-web-somescreen/demo/src/main/java/com/example/demo/DemoController.java
+++ spring-boot-web-somescreen/demo/src/main/java/com/example/demo/DemoController.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 /**
