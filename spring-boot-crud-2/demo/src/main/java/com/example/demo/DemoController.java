c234d38f818e42cb953a166158f1758fe7d10b45
purin-it <stanahas@amber.plala.or.jp>
2020-08-09 14:56:27 +0900

    spring-boot-crud-2

--- spring-boot-crud-2/demo/src/main/java/com/example/demo/DemoController.java
+++ spring-boot-crud-2/demo/src/main/java/com/example/demo/DemoController.java
@@ -6,7 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.support.SessionStatus;
 
@@ -186,7 +191,7 @@ public class DemoController {
             //更新・追加処理
             repository.saveAndFlush(userData);
         }
-        return normalPath;
+        return checkOthersPath;
     }
 
     /**
