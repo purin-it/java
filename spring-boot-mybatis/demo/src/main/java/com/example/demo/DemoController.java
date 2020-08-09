1eb2c90ad2690b3c47efb97727555a35b58ee0b2
purin-it <stanahas@amber.plala.or.jp>
2020-08-09 14:51:19 +0900

    spring-boot-mybatis

--- spring-boot-mybatis/demo/src/main/java/com/example/demo/DemoController.java
+++ spring-boot-mybatis/demo/src/main/java/com/example/demo/DemoController.java
@@ -5,7 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.support.SessionStatus;
 
