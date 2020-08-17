01b91328b9ddf924c5950693d2d53b94fe9b5bc0
purin-it <stanahas@amber.plala.or.jp>
2020-08-17 21:29:55 +0900

    spring-boot-aop-annotation

--- spring-boot-aop-annotation/demo/src/main/java/com/example/demo/DemoController.java
+++ spring-boot-aop-annotation/demo/src/main/java/com/example/demo/DemoController.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @DemoAnnotation
@@ -11,7 +11,7 @@ public class DemoController {
      * 初期表示画面に遷移する
      * @return 初期表示画面へのパス
      */
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(){
         return "index";
     }
@@ -20,7 +20,7 @@ public class DemoController {
      * メソッドに独自アノテーションが有る場合の処理
      * @return 初期表示画面へのパス
      */
-    @RequestMapping("/has_method_annotation")
+    @GetMapping("/has_method_annotation")
     @DemoAnnotation
     public String has_method_annotation(){
         return "index";
@@ -30,7 +30,7 @@ public class DemoController {
      * メソッドに独自アノテーションが無い場合の処理
      * @return 初期表示画面へのパス
      */
-    @RequestMapping("/has_no_method_annotation")
+    @GetMapping("/has_no_method_annotation")
     public String has_no_method_annotation(){
         return "index";
     }
