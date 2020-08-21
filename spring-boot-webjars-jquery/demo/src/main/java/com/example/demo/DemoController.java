761d315e4513cccdf67175960acd538ccdefeef6
purin-it <stanahas@amber.plala.or.jp>
2020-08-21 09:21:28 +0900

    spring-boot-webjars-jquery

--- spring-boot-webjars-jquery/demo/src/main/java/com/example/demo/DemoController.java
+++ spring-boot-webjars-jquery/demo/src/main/java/com/example/demo/DemoController.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.thymeleaf.util.StringUtils;
@@ -28,7 +28,7 @@ public class DemoController {
      * 初期表示画面に遷移する
      * @return 初期表示画面へのパス
      */
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(){
         return "index";
     }
@@ -38,7 +38,7 @@ public class DemoController {
      * @return ユーザーデータリスト(JSON形式)
      */
     //JSON文字列を返却するために、@ResponseBodyアノテーションを付与
-    @RequestMapping("/search")
+    @GetMapping("/search")
     @ResponseBody
     public String search(){
         // ユーザーデータをIDの昇順に取得し、取得できなければそのまま返す
@@ -66,7 +66,7 @@ public class DemoController {
      */
     private String encode(String data){
         if(StringUtils.isEmpty(data)){
-            return "";
+            return data;
         }
         String retVal = null;
         try{
