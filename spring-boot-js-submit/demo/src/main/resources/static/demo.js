93759a14ff32d2c8aebbc6f92d8e840c3ba63574
purin-it <stanahas@amber.plala.or.jp>
2020-08-29 11:40:59 +0900

    spring-boot-js-submit

--- spring-boot-js-submit/demo/src/main/resources/static/demo.js
+++ spring-boot-js-submit/demo/src/main/resources/static/demo.js
@@ -1,6 +1,14 @@
 // 引数で指定されたパスでサブミットする
 function formSubmit(path){
+    if(!path){
+        alert('パスを指定してください');
+        return;
+    }
     var form = document.getElementsByTagName('form')[0];
+    if(!form){
+        alert('フォームが取得できませんでした');
+        return;
+    }
     form.action=path;
     form.method="post";
     form.submit();
