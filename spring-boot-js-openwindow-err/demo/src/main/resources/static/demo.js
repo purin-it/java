5b5f9d519d067e9b0dd2e596a103feeb22346e8e
purin-it <stanahas@amber.plala.or.jp>
2020-09-01 21:17:10 +0900

    spring-boot-js-openwindow-err

--- spring-boot-js-openwindow-err/demo/src/main/resources/static/demo.js
+++ spring-boot-js-openwindow-err/demo/src/main/resources/static/demo.js
@@ -1,31 +1,31 @@
 //フォーム名を指定して新しいウィンドウを開く
 function openWin(formName){
-    var form = document.forms[formName];
+    let form = document.forms[formName];
     if(!form){
         alert('指定したフォームが取得できませんでした');
         return;
     }
-    var win = window.open('about:blank', formName);
+    let win = window.open('about:blank', formName);
     form.target = formName;
     form.submit();
     win.focus();
 }
 //子画面のエラーメッセージを親画面に転記
 function printOpenErrMsg(){
-    var errList = document.getElementById('errList');
+    let errList = document.getElementById('errList');
     if(errList){
-        var errMessages = errList.getElementsByTagName('li');
+        let errMessages = errList.getElementsByTagName('li');
         //子画面にエラーメッセージがある場合
         if(errMessages && errMessages.length > 0){
             //親画面のエラーメッセージリストを取得し初期化後、メッセージを追加
-            var errListP = window.opener.document.getElementById('errList');
+            let errListP = window.opener.document.getElementById('errList');
             if(errListP){
                 errListP.innerHTML = "";
-                errListUlP = document.createElement("ul");
+                let errListUlP = document.createElement("ul");
                 errListP.appendChild(errListUlP)
-                for(var i = 0; i < errMessages.length; i++){
-                    var msg = errMessages[i].textContent;
-                    errListLiP = document.createElement("li");
+                for(let i = 0; i < errMessages.length; i++){
+                    let msg = errMessages[i].textContent;
+                    let errListLiP = document.createElement("li");
                     errListLiP.innerHTML = msg;
                     errListLiP.classList.add('errorMessage');
                     errListUlP.appendChild(errListLiP);
