a0b53b37c4f408ac04a39fc811f63951f7542d25
purin-it <stanahas@amber.plala.or.jp>
2022-03-03 22:37:11 +0900

    spring-boot-rest-api-check

---
+++ spring-boot-rest-api-check/demoRestApi/src/main/java/com/example/demo/DemoRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestApiApplication.class, args);
+	}
+
+}
