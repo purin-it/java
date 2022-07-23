6c73fc3c5850fcca80e3bf723f75bf1ec0417821
purin-it <stanahas@amber.plala.or.jp>
2022-07-23 23:56:23 +0900

    spring-boot-oracle-performance-between

--- spring-boot-oracle-performance-between/demo/src/main/java/com/example/demo/DemoService.java
+++ spring-boot-oracle-performance-between/demo/src/main/java/com/example/demo/DemoService.java
@@ -3,67 +3,29 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Calendar;
-import java.util.List;
-
 @Service
 public class DemoService {
 
     /**
-     * ユーザーデータテーブル(user_data)へアクセスするマッパー
+     * 性能測定を行うための個別サービス
      */
     @Autowired
-    private UserDataMapper userDataMapper;
+    private DemoServiceSub demoServiceSub;
 
     /**
-     * ユーザーデータテーブル(user_data)を更新するトランザクション
+     * 性能検証を行うためのサービス
      */
     public void verifyPerformance() {
         System.out.println("com.example.demo.DemoService.verifyPerformance start.");
         System.out.println();
 
-        // 初回DB接続時は時間がかかるため、あえて性能測定対象外のSQLを1度実行しておく
-        userDataMapper.findById(Long.valueOf(1));
+        System.out.println("=== 1回目の性能測定を行います. ===");
+        demoServiceSub.verifyPerformanceEach();
         System.out.println();
 
-        System.out.println("--- ユーザーデータテーブル(user_data)から生年月日が基準日前後10日間の"
-                + "データを、BETWEEN句を使わないで全件取得するSQLを実行します start. ---");
-
-        // 処理前の現在時刻を取得
-        long startTime = System.currentTimeMillis();
-
-        // 生年月日が基準日(2012/1/20)前後10日間のデータを、BETWEEN句を使わないで全件取得する
-        Calendar cal = Calendar.getInstance();
-        cal.set(2012, 0, 20);
-        List<UserData> userDataList = userDataMapper.findByBirthdayNotBetween(cal.getTime());
-
-        // 処理後の現在時刻を取得
-        long endTime = System.currentTimeMillis();
-
-        System.out.println("取得件数 : " + userDataList.size());
-        System.out.println("処理時間：" + (endTime - startTime) + " ms");
-
-        System.out.println("--- ユーザーデータテーブル(user_data)から生年月日が基準日前後10日間の"
-                + "データを、BETWEEN句を使わないで全件取得するSQLを実行します end. ---");
+        System.out.println("=== 2回目の性能測定を行います. ===");
+        demoServiceSub.verifyPerformanceEach();
         System.out.println();
-
-        System.out.println("--- ユーザーデータテーブル(user_data)から生年月日が基準日前後10日間の"
-                + "データを、BETWEEN句を使って全件取得するSQLを実行します start. ---");
-
-        // 処理前の現在時刻を取得
-        startTime = System.currentTimeMillis();
-
-        // 生年月日が基準日(2012/1/20)前後10日間のデータを、BETWEEN句を使って全件取得する
-        userDataList = userDataMapper.findByBirthdayBetween(cal.getTime());
-
-        // 処理後の現在時刻を取得
-        endTime = System.currentTimeMillis();
-
-        System.out.println("取得件数 : " + userDataList.size());
-        System.out.println("処理時間：" + (endTime - startTime) + " ms");
-
-        System.out.println("--- ユーザーデータテーブル(user_data)から生年月日が基準日前後10日間の"
-                + "データを、BETWEEN句を使って全件取得するSQLを実行します end. ---");
 
         System.out.println("com.example.demo.DemoService.verifyPerformance end.");
     }
---
+++ spring-boot-oracle-performance-between/demo/src/main/java/com/example/demo/DemoServiceSub.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.List;
+
+@Service
+public class DemoServiceSub {
+
+    /**
+     * ユーザーデータテーブル(user_data)へアクセスするマッパー
+     */
+    @Autowired
+    private UserDataMapper userDataMapper;
+
+    /**
+     * 性能検証を行うための個別サービス
+     */
+    public void verifyPerformanceEach() {
+        // 初回DB接続時は時間がかかるため、あえて性能測定対象外のSQLを1度実行しておく
+        userDataMapper.findById(Long.valueOf(1));
+
+        System.out.println("--- ユーザーデータテーブル(user_data)から生年月日が基準日前後10日間の"
+                + "データを、BETWEEN句を使わないで全件取得するSQLを実行します start. ---");
+
+        // 処理前の現在時刻を取得
+        long startTime = System.currentTimeMillis();
+
+        // 生年月日が基準日(2012/1/20)前後10日間のデータを、BETWEEN句を使わないで全件取得する
+        Calendar cal = Calendar.getInstance();
+        cal.set(2012, 0, 20);
+        List<UserData> userDataList = userDataMapper.findByBirthdayNotBetween(cal.getTime());
+
+        // 処理後の現在時刻を取得
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("取得件数 : " + userDataList.size());
+        System.out.println("処理時間：" + (endTime - startTime) + " ms");
+
+        System.out.println("--- ユーザーデータテーブル(user_data)から生年月日が基準日前後10日間の"
+                + "データを、BETWEEN句を使わないで全件取得するSQLを実行します end. ---");
+        System.out.println();
+
+        System.out.println("--- ユーザーデータテーブル(user_data)から生年月日が基準日前後10日間の"
+                + "データを、BETWEEN句を使って全件取得するSQLを実行します start. ---");
+
+        // 処理前の現在時刻を取得
+        startTime = System.currentTimeMillis();
+
+        // 生年月日が基準日(2012/1/20)前後10日間のデータを、BETWEEN句を使って全件取得する
+        userDataList = userDataMapper.findByBirthdayBetween(cal.getTime());
+
+        // 処理後の現在時刻を取得
+        endTime = System.currentTimeMillis();
+
+        System.out.println("取得件数 : " + userDataList.size());
+        System.out.println("処理時間：" + (endTime - startTime) + " ms");
+
+        System.out.println("--- ユーザーデータテーブル(user_data)から生年月日が基準日前後10日間の"
+                + "データを、BETWEEN句を使って全件取得するSQLを実行します end. ---");
+    }
+}
